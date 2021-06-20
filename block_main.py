9e567c90f370ba0e22ec29c3c9faff499ec5d53a
sabi <codespectator@gmail.com>
2021-06-20 14:38:56 +0900

    M: bug fix

--- block_main.py
+++ block_main.py
@@ -7,60 +7,68 @@ import cv2
 import time
 import ast
 from pycocotools import mask as m
+from torchvision.io.image import read_file, decode_jpeg
+
 
 def make_dataset_attr():
     root = '/home/sabi/workspace/ganhackerton/dataset'
     with os.scandir(root) as entries:
         fpathes = [os.path.join(root, fname) for fname in entries if os.path.isfile(fname)]
-    dfall = pd.concat([pd.read_csv(fpath) for fpath in fpathes[:1]]).drop('Unnamed: 0',axis=1)
+    dfall = pd.concat([pd.read_csv(fpath) for fpath in fpathes[:2]]).drop('Unnamed: 0',axis=1)
     basename = '/main/dataset'
     pts = np.zeros((1000,2))
     tmp = np.zeros((512,512),dtype=np.uint8)
     keys = [ k for k in dfall.columns if 'exif' not in k and 'polygon' not in k]
     utf8_type = hf.string_dtype('utf-8')
     start = time.time()
+    with open('datasetInfo.txt', 'w') as dataSetInfo:
+        dataSetInfo.write('*.h5 file Structure:\n')
+        dataSetInfo.write('Each group has 500 dataset, each of the dataset has an image in bytes format and its labels in attributes.\n')
+        dataSetInfo.write('An image in dataset is stored as bytes so it is needed to be decode.\n')
+        dataSetInfo.write('image size: 512x512x3\n')
+        #dfall.shape[0]//500
+        for j in range(dfall.shape[0]//500):
+            fname = 'dataSet{0:03d}.h5'.format(j)
+            dsname = os.path.join('/home/sabi/workspace/datablock/dataset', fname)
+            dataSetInfo.write(fname+'\n')
+            hfg = hf.File(dsname, 'w')
+            grp = hfg.create_group('group{0:03d}'.format(j))
+            for i in range(500*j, 500*(j+1)):
+                item = dfall.iloc[i]
+                with open(item['path'].replace(basename, root), 'rb') as f:
+                    bimg = f.read()
+                    dset = grp.create_dataset('{}'.format(i), data=np.frombuffer(bimg, dtype=np.uint8))
+                    try:
+                        for n in [1,2]:
+                            name = 'polygon{}'.format(n)
+                            tmp.fill(0)
+                            poly = ast.literal_eval(item[name])
+                            pl_list =[]
+                            for pl in poly:
+                                j = 0
+                                for p in pl:
+                                    x, y = p['x'], p['y']
+                                    pts[j] = x, y
+                                    j += 1
+                                pl_list.append(pts[:j].astype(np.int32))
+                            cv2.fillPoly(tmp, pl_list, 255, lineType=cv2.LINE_AA)
+                            mask = np.array(tmp > 0, dtype=bool, order='F')
+                            rle_mask = m.encode(mask)
+                            dset.attrs[name] = str(rle_mask).encode('utf-8')
+                    except KeyError as e:
+                        pass
 
-    for j in range(4):
-        dsname = '/home/sabi/workspace/datablock/dataset/dataSet{}.h5'.format(j)
-        hfg = hf.File(dsname, 'w')
-        grp = hfg.create_group('group{0:05d}'.format(j))
-        for i in range(500*j,500*(j+1)):
-            subgrp = grp.create_group('{}'.format(i))
-            item = dfall.iloc[i]
-            with open(item['path'].replace(basename, root), 'rb') as f:
-                bimg = f.read()
-                dset = subgrp.create_dataset('image', data=np.frombuffer(bimg, dtype=np.uint8))
-                try:
-                    for n in [1,2]:
-                        name = 'polygon{}'.format(n)
-                        tmp.fill(0)
-                        poly = ast.literal_eval(item[name])
-                        pl_list =[]
-                        for pl in poly:
-                            j = 0
-                            for p in pl:
-                                x, y = p['x'], p['y']
-                                pts[j] = x, y
-                                j += 1
-                            pl_list.append(pts[:j].astype(np.int32))
-                        cv2.fillPoly(tmp, pl_list, 255, lineType=cv2.LINE_AA)
-                        mask = np.array(tmp > 0, dtype=bool, order='F')
-                        rle_mask = m.encode(mask)
-                        dset.attrs[name] = np.void(str(rle_mask).encode('utf-8'))
-                except KeyError as e:
-                    pass
+                    for k in keys:
+                        data = item[k]
+                        dset.attrs[k] = np.array(data, dtype = utf8_type if isinstance(data, str) else float)
 
-                for k in keys:
-                    data = item[k]
-                    dset.attrs[k] = np.array(data, dtype = utf8_type if isinstance(data, str) else float)
-
-        hfg.close()
+            hfg.close()
 
 def make_numpy_data_set():
     root = '/home/sabi/workspace/ganhackerton/dataset'
     with os.scandir(root) as entries:
         fpathes = [os.path.join(root, fname) for fname in entries if os.path.isfile(fname)]
-    dfall = pd.concat([pd.read_csv(fpath) for fpath in fpathes[:1]]).drop('Unnamed: 0', axis=1)
+    dfall = pd.concat([pd.read_csv(fpath) for fpath in fpathes[:2]]).drop('Unnamed: 0', axis=1)
     basename = '/main/dataset'
     pts = np.zeros((1000, 2))
     tmp = np.zeros((512, 512), dtype=np.uint8)
@@ -68,7 +76,7 @@ def make_numpy_data_set():
     utf8_type = hf.string_dtype('utf-8')
     start = time.time()
 
-    for j in range(4):
+    for j in range(dfall.shape[0]//500):
         dsname = '/home/sabi/workspace/datablock/dataset/dataSet{}.h5'.format(j)
         hfg = hf.File(dsname, 'w')
         dataset = hfg.create_dataset('image',(500,512,512,4),dtype=np.uint8)
@@ -113,6 +121,7 @@ def make_numpy_data_set():
         hfg.close()
 
 if __name__=='__main__':
+    start = time.time()
     if 1:
         make_dataset_attr()
     else:
--- hdf5Dataset.py
+++ hdf5Dataset.py
@@ -11,6 +11,41 @@ import ast
 import timeit
 import time
 from PIL import Image
+import pandas as pd
+
+class FileDataset(data.Dataset):
+    def __init__(self):
+        super().__init__()
+        fpathes = ['/home/sabi/workspace/ganhackerton/dataset/dataindex_07.csv', '/home/sabi/workspace/ganhackerton/dataset/dataindex_24.csv']
+        self.dfall = pd.concat([pd.read_csv(fpath) for fpath in fpathes]).drop('Unnamed: 0', axis=1)
+        self.basename = '/main/dataset'
+        self.totalData = self.dfall.shape[0]
+        self.data_info = []
+        self.pts = np.zeros((1000, 2))
+        self.root = '/home/sabi/workspace/ganhackerton/dataset'
+
+    def __len__(self):
+        return self.totalData
+
+    def __getitem__(self, item):
+        data =self.dfall.iloc[item]
+        path=data['path'].replace(self.basename,self.root)
+        img = cv2.imread(path)
+        tmp = np.zeros(img.shape[:2])
+        for n in [1, 2]:
+            name = 'polygon{}'.format(n)
+            poly = ast.literal_eval(data[name])
+            pl_list = []
+            for pl in poly:
+                j = 0
+                for p in pl:
+                    x, y = p['x'], p['y']
+                    self.pts[j] = x, y
+                    j += 1
+                pl_list.append(self.pts[:j].astype(np.int32))
+            cv2.fillPoly(tmp, pl_list, n, lineType=cv2.LINE_AA)
+        return {'image':img, 'mask':tmp
+                }
 class Hdf5Dataset(data.Dataset):
     """
 
@@ -29,13 +64,13 @@ class Hdf5Dataset(data.Dataset):
     def __getitem__(self, item):
         fidx = item//500
         with h5py.File(self.files[fidx]) as f:
-            dataset = f['group{0:05}'.format(fidx)]['{}'.format(item)]['image']
+            dataset = f['group{0:03}'.format(fidx)]['{}'.format(item)]#f['group{0:05}'.format(fidx)]['{}'.format(item)]['image']
             data ={
                 'image':  cv2.imdecode(np.array(dataset), flags=cv2.IMREAD_COLOR)
             }
             for k in ['polygon1', 'polygon2']:
                 if k in data.keys():
-                    data[k]=m.decode(ast.literal_eval(data[k]))#.tobytes().decode('utf-8')))
+                    data[k]=m.decode(ast.literal_eval(data[k]))
             # for k, val in dataset.attrs.items():
             #     data[k] = val
             #img = data['image']
@@ -94,9 +129,10 @@ def func():
     # 10 iteration total excution time for bytes type polygon store:125
     # 10 iter total excution time for bytes with worker 4: 49 sec
     device = torch.device("cuda")
-    root = '/home/sabi/workspace/datablock/dataset_strings'
+    root = '/home/sabi/workspace/datablock/dataset'
     dataset = Hdf5Dataset(root)
     #dataset = Hdf5DatasetNumPy(root)
+    #dataset = FileDataset()
     loader = data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2,pin_memory=True)
 
     for i in loader:
@@ -107,6 +143,6 @@ def func():
         # print(data.keys())
 if __name__ == "__main__":
 
-    ti = timeit.timeit(func,number=10)
+    ti = timeit.timeit(func,number=1)
     print(ti)
 
